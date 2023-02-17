8812c4bedef115ccfc8028129b7dde5115c242a0
Jean Tapias <tranx6@gmail.com>
2023-02-17 08:24:28 -0800

    2.0.22.next

--- src/main/java/com/myzlab/k/SqlEncoding.java
+++ src/main/java/com/myzlab/k/SqlEncoding.java
@@ -1,20 +1,200 @@
 package com.myzlab.k;
 
-import com.myzlab.k.sql.format.KBase64;
-import com.myzlab.k.sql.format.KEscape;
-import com.myzlab.k.sql.format.KHex;
+import com.myzlab.k.sql.encoding.KBig5;
+import com.myzlab.k.sql.encoding.KEucCn;
+import com.myzlab.k.sql.encoding.KEucJis2004;
+import com.myzlab.k.sql.encoding.KEucJp;
+import com.myzlab.k.sql.encoding.KEucKr;
+import com.myzlab.k.sql.encoding.KEucTw;
+import com.myzlab.k.sql.encoding.KGb18030;
+import com.myzlab.k.sql.encoding.KGbk;
+import com.myzlab.k.sql.encoding.KIso88595;
+import com.myzlab.k.sql.encoding.KIso88596;
+import com.myzlab.k.sql.encoding.KIso88597;
+import com.myzlab.k.sql.encoding.KIso88598;
+import com.myzlab.k.sql.encoding.KJohab;
+import com.myzlab.k.sql.encoding.KKoi8r;
+import com.myzlab.k.sql.encoding.KKoi8u;
+import com.myzlab.k.sql.encoding.KLatin1;
+import com.myzlab.k.sql.encoding.KLatin2;
+import com.myzlab.k.sql.encoding.KLatin3;
+import com.myzlab.k.sql.encoding.KLatin4;
+import com.myzlab.k.sql.encoding.KLatin5;
+import com.myzlab.k.sql.encoding.KLatin6;
+import com.myzlab.k.sql.encoding.KLatin7;
+import com.myzlab.k.sql.encoding.KLatin8;
+import com.myzlab.k.sql.encoding.KLatin9;
+import com.myzlab.k.sql.encoding.KMuleInternal;
+import com.myzlab.k.sql.encoding.KShiftJis2004;
+import com.myzlab.k.sql.encoding.KSjis;
+import com.myzlab.k.sql.encoding.KSqlAscii;
+import com.myzlab.k.sql.encoding.KUhc;
+import com.myzlab.k.sql.encoding.KUtf8;
+import com.myzlab.k.sql.encoding.KWin;
+import com.myzlab.k.sql.encoding.KWin1250;
+import com.myzlab.k.sql.encoding.KWin1251;
+import com.myzlab.k.sql.encoding.KWin1252;
+import com.myzlab.k.sql.encoding.KWin1253;
+import com.myzlab.k.sql.encoding.KWin1254;
+import com.myzlab.k.sql.encoding.KWin1255;
+import com.myzlab.k.sql.encoding.KWin1256;
+import com.myzlab.k.sql.encoding.KWin866;
 
-public class SqlFormat {
+public class SqlEncoding {
     
-    public static KBase64 base64() {
-        return new KBase64();
+    public static KBig5 big5() {
+        return new KBig5();
     }
     
-    public static KEscape escape() {
-        return new KEscape();
+    public static KEucCn eucCn() {
+        return new KEucCn();
     }
     
-    public static KHex hex() {
-        return new KHex();
+    public static KEucJis2004 eucJis2004() {
+        return new KEucJis2004();
+    }
+    
+    public static KEucJp eucJp() {
+        return new KEucJp();
+    }
+    
+    public static KEucKr eucKr() {
+        return new KEucKr();
+    }
+    
+    public static KEucTw eucTw() {
+        return new KEucTw();
+    }
+    
+    public static KGb18030 gb18030() {
+        return new KGb18030();
+    }
+    
+    public static KGbk gbk() {
+        return new KGbk();
+    }
+    
+    public static KIso88595 iso88595() {
+        return new KIso88595();
+    }
+    
+    public static KIso88596 iso88596() {
+        return new KIso88596();
+    }
+    
+    public static KIso88597 iso88597() {
+        return new KIso88597();
+    }
+    
+    public static KIso88598 iso88598() {
+        return new KIso88598();
+    }
+    
+    public static KJohab johab() {
+        return new KJohab();
+    }
+    
+    public static KKoi8r koi8r() {
+        return new KKoi8r();
+    }
+    
+    public static KKoi8u koi8u() {
+        return new KKoi8u();
+    }
+    
+    public static KLatin1 KLatin1() {
+        return new KLatin1();
+    }
+    
+    public static KLatin2 KLatin12() {
+        return new KLatin2();
+    }
+    
+    public static KLatin3 KLatin3() {
+        return new KLatin3();
+    }
+    
+    public static KLatin4 KLatin4() {
+        return new KLatin4();
+    }
+    
+    public static KLatin5 KLatin5() {
+        return new KLatin5();
+    }
+    
+    public static KLatin6 KLatin6() {
+        return new KLatin6();
+    }
+    
+    public static KLatin7 KLatin7() {
+        return new KLatin7();
+    }
+    
+    public static KLatin8 KLatin8() {
+        return new KLatin8();
+    }
+    
+    public static KLatin9 KLatin9() {
+        return new KLatin9();
+    }
+    
+    public static KMuleInternal muleInternal() {
+        return new KMuleInternal();
+    }
+    
+    public static KShiftJis2004 shiftJis2004() {
+        return new KShiftJis2004();
+    }
+    
+    public static KSjis sjis() {
+        return new KSjis();
+    }
+    
+    public static KSqlAscii sqlAscii() {
+        return new KSqlAscii();
+    }
+    
+    public static KUhc uhc() {
+        return new KUhc();
+    }
+    
+    public static KUtf8 utf8() {
+        return new KUtf8();
+    }
+    
+    public static KWin win() {
+        return new KWin();
+    }
+    
+    public static KWin1250 win1250() {
+        return new KWin1250();
+    }
+    
+    public static KWin1251 win1251() {
+        return new KWin1251();
+    }
+    
+    public static KWin1252 win1252() {
+        return new KWin1252();
+    }
+    
+    public static KWin1253 win1253() {
+        return new KWin1253();
+    }
+    
+    public static KWin1254 win1254() {
+        return new KWin1254();
+    }
+    
+    public static KWin1255 win1255() {
+        return new KWin1255();
+    }
+    
+    public static KWin1256 win1256() {
+        return new KWin1256();
+    }
+    
+    public static KWin866 win866() {
+        return new KWin866();
     }
 }
