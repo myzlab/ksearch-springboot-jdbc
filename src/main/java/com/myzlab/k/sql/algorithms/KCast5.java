0e450fb98f9dc4a6525a95c57c0a9cf3009426fa
Jean Tapias <tranx6@gmail.com>
2023-06-29 19:48:17 -0700

    Added new SQL algorithms

---
+++ src/main/java/com/myzlab/k/sql/algorithms/KCast5.java
@@ -0,0 +1,11 @@
+package com.myzlab.k.sql.algorithms;
+
+import com.myzlab.k.KAlgorithm;
+
+public class KXdes extends KAlgorithm {
+    
+    @Override
+    public String toSql() {
+        return "xdes";
+    }
+}
